7809bc3f3aa00c1e0b2e2fc3971d6da7264196bd
wem-r <57370095+wem-r@users.noreply.github.com>
2022-04-27 18:09:07 +0200

    Update WoL-GUI.py

--- WoL-GUI.py
+++ WoL-GUI.py
@@ -1,9 +1,10 @@
-from asyncio import subprocess
 from distutils import cmd
+import sys
 from tkinter import *
 import struct, socket
 import os
-from subprocess import Popen, PIPE
+#import subprocess
+#from subprocess import Popen, PIPE
 from sqlalchemy import true
      
 class sim(Frame):
@@ -12,13 +13,15 @@ class sim(Frame):
         self.master = master
         self.pack(fill=BOTH, expand=1)
         myfont = family = ("Century Gothic", 8)
+        Green = "#00FF80"
+        Red = '#F44336'
         # White Square Block
         canvas = Canvas(self, bg="#222529")
         #canvas.create_rectangle(10, 20, 630, 200, width=1, outline="white", fill='#222529')
         canvas.pack(fill=BOTH, expand=1)
         # Text Label
-        self.label = Label(master, font=("Century Gothic", 9), text="MAC Address", bg='#222529', fg='white')
-        self.label.place(x=45, y=10)
+        self.label = Label(master, font=("Century Gothic", 9), text="MAC Address or IP", bg='#222529', fg='white')
+        self.label.place(x=35, y=10)
 
         # Wake-On-LAN
         #
@@ -46,46 +49,55 @@ class sim(Frame):
             s.close()
 
         def arp_req():
+            print('')
             os.system("arp -a")
-            
+            print('')
+
+
+
         def GetMacInput():
             mac_inp = macbox.get(1.0, "end-1c")
             if mac_inp:
                 WakeOnLan(mac_inp)
-                print("Waking up "+mac_inp)
+                cmdbox.insert('end', "Waking up "+mac_inp+'\n')
             else:
-                print('You need to enter a MAC Address')
+                cmdbox.insert('end', f"{Red}You need to enter a MAC Address first\nseparated by ':' not '-'\n")
             #WakeOnLan(macaddr)
-        def clear_cmdbox():
-            cmdbox.delete('end')
-            print('cleared')
+
+        def GetIPInput():
+            ip_inp = macbox.get(1.0, "end-1c")
+            if ip_inp:
+                arp_a = os.system("arp -a "+ ip_inp)
+                cmdbox.insert('end', arp_a)
+            else:
+                print("\nYou need to enter an IP Address first\n")
+            #WakeOnLan(macaddr)
+
+        def ClearCmdbox():
+            cmdbox.delete(1.0, 'end')
 
         # Button
         self.button1 = Button(root, font=myfont, bg='#dbdbdb', text="Wake Up", command=GetMacInput, height=1, width=19, borderwidth=0, relief=SOLID)
         self.button1.place(x=10, y=65)
-        self.button2 = Button(root, font=myfont, bg='#dbdbdb', text="arp -a", command=arp_req, height=1, width=19, borderwidth=0, relief=SOLID)
+        self.button2 = Button(root, font=myfont, bg='#dbdbdb', text="arp -a", command=arp_req, height=1, width=7, borderwidth=0, relief=SOLID)
         self.button2.place(x=10, y=100)
-        self.button3 = Button(root, font=myfont, bg='#dbdbdb', text="Clear Output", command=clear_cmdbox, height=1, width=19, borderwidth=0, relief=SOLID)
+        self.button2 = Button(root, font=myfont, bg='#dbdbdb', text="arp -S <IP>", command=GetIPInput, height=1, width=7, borderwidth=0, relief=SOLID)
+        self.button2.place(x=92, y=100)
+        self.button3 = Button(root, font=myfont, bg='#dbdbdb', text="Clear Output", command=ClearCmdbox, height=1, width=19, borderwidth=0, relief=SOLID)
         self.button3.place(x=10, y=185)
 
         macbox = Text(root, height=1, width=19, relief="sunken")
         macbox.place(x=10, y=30)
         
-        output = "Output testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"
-        cmdbox = Text(root, height=13, width=63, relief="sunken", wrap='char', font=('Consolas', 9), bg='#000000', fg='#00FF80')
+        #output = "Waking up "
+        cmdbox = Text(root, height=13, width=63, relief="sunken", wrap='char', font=('Consolas', 9), bg='#000000', fg=Green)
         cmdbox.place(x=180, y=10)
-        cmdbox.insert('end', output)
-        cmdbox.config(state='disabled')
+        #cmdbox.config(state='disabled')
+
         #sb = Scrollbar(root)
         #sb.pack(side=RIGHT, fill=BOTH)
         #cmdbox.config(yscrollcommand=sb.set)
         #sb.config(command=cmdbox.yview)
-        
-
-        #with subprocess.Popen("ls -la", shell=True, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
-        #    for line in p.stdout:
-        #        cmdbox.insert(tk.END, line)
-    
         
 
 root = Tk()
